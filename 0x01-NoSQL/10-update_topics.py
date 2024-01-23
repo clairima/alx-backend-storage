c027216ae0f86c45cbe65bb01d2637b0f972fa4d
clairima <mertahrima@gmail.com>
2024-01-23 06:09:35 -0800

    tasks 5-11

---
+++ 0x01-NoSQL/10-update_topics.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""change school"""
+import pymongo
+
+
+def update_topics(mongo_collection, name, topics):
+    """
+    update many rows
+    """
+    return mongo_collection.update_many(
+        {"name": name},
+        {"$set": {"topics": topics}}
+    )
